6d29b3f2db5a0f347f3d54d57dcdb52d25461f89
Debojyoti Roy <125260380+DEBOROY-1234@users.noreply.github.com>
2025-08-23 11:00:36 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 565463
+let accountEmail = "deboroy@gmail.com"
+var accountPassword = "738383"
+accountCity = "Kolkata"
+let accountState
+
+accountEmail = "hehehe@gmail"
+accountPassword = "hfhfhfh"
+accountCity = "delhi"
+
+/*
+    prefer not to use var bcz of issue in block and functional scope
+*/
+
+console.table([accountEmail,accountPassword,accountId,accountCity,accountState]);
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,3 @@
+"use strict" //treat all JS code as newer versions
+
+// alert(3+3)     this code iwll not work bcz we are using nodejs not JS on browser console
