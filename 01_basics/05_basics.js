f15892ef534dcb1a1f2b04c48409b0ee7b331d3f
Debojyoti Roy <125260380+DEBOROY-1234@users.noreply.github.com>
2025-08-24 15:14:32 +0000

    commit

--- 01_basics/05_basics.js
+++ 01_basics/05_basics.js
@@ -1,6 +1,22 @@
-const name = "debo"
+const name = "debko"
 const num = 90090
 
-const newName = new String(`deboj`)
+const newName = new String(`                            debkdsasasaso`)
 
-console.log(`hello my name is ${newName} and i won ${num} dollars`);
+//console.log(`hello my name is ${newName} and i won ${num} dollars`);
+// console.log(typeof name);
+console.log(newName);
+console.log(newName.trim());
+console.log(newName.replace('sasasa','-'));
+console.log(newName.includes('sas'));
+
+
+
+// console.log(newName.charAt('4'));
+// console.log(newName.indexOf('o'));
+
+//const sub = newName.substring(2,5)
+
+//console.log(sub);
+
+
