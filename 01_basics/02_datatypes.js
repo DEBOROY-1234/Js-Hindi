1f9d94851777fd3aafdf80919abe612cbc3ab028
Debojyoti Roy <125260380+DEBOROY-1234@users.noreply.github.com>
2025-08-24 14:48:41 +0000

    commit

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,13 @@
 "use strict" //treat all JS code as newer versions
 
 // alert(3+3)     this code iwll not work bcz we are using nodejs not JS on browser console
+let userOne = {
+    email : "hello"
+}
+
+let userTwo = userOne
+userTwo.email = "yooo"
+
+console.log(userOne.email);
+
+
---
+++ 01_basics/05_basics.js
@@ -0,0 +1,6 @@
+const name = "debo"
+const num = 90090
+
+const newName = new String(`deboj`)
+
+console.log(`hello my name is ${newName} and i won ${num} dollars`);
